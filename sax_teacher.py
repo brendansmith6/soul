1dca8bdb0c474f91cc874c407494518037688622
brendansmith6 <44763891+brendansmith6@users.noreply.github.com>
2018-11-07 00:53:59 -0700

    Add files via upload

--- sax_teacher.py
+++ sax_teacher.py
@@ -1,7 +1,7 @@
 # -----------------------------------------+
 # Brendan Smith                            |
 # -----------------------------------------|
-# Program that helps me learn              |
+# Program that helps me learn python and   |
 # how to play the saxophone                |
 # -----------------------------------------+
 
@@ -10,8 +10,7 @@ POSSIBLE_NOTES = ['A','a', 'A#', 'a#', 'B', 'b', 'C', 'c', 'C#', 'c#', 'D',
                   'd','D#','d#', 'E','e', 'F','f', 'F#','f#','G','g', 'G#', 'g#']
 
 #Simple Fingering Chart
-simple_fingering_chart = ["B = 1\n",
-                          "C = (2)\n",
+simple_fingering_chart = ['B = 1\n',
                           "A = 2\n"
                           "G = 3\n"
                           "F = 4\n"
@@ -23,11 +22,23 @@ make_staff = ["_________________________________________________________________
               "____________________________________________________________________\n",
               "____________________________________________________________________\n",
               "____________________________________________________________________\n",
-              "____________________________________________________________________\n"]
+              "___________________________________________________________________\n"]
 
 print(POSSIBLE_NOTES)
 print(simple_fingering_chart)
 print(make_staff)
 
+q1 = input("what size reid should you have when starting off?")
+if(q1 == 2 | 2.5 | 3):
+    print("correct sir")
+q2 = input("true or false: its good to soak your reid in amber listerine")
+if (q2 == "true"):
+    print("yes!")
+else:
+    ("no sir")
+
+q3 = input("what is the fingering chart")
+if(q3 == "BAGFED"):
+    print("gold star***")
 
 
